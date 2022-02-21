c3d31ebf5716bc4ed118a71d097e69b2e63cae06
slope119 <98922380+slope119@users.noreply.github.com>
2022-02-21 01:38:16 -0300

    Add files via upload

---
+++ 2.2.1/PaintApp.java
@@ -0,0 +1,80 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class PaintApp {
+    public static void main (String[] args) {
+        PaintFrame frame = new PaintFrame();
+        frame.setVisible(true);
+    }
+}
+
+class PaintFrame extends JFrame {
+    Rect r1,r2,r3;
+
+    PaintFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        this.setTitle("Painting Figures");
+        this.setSize(350, 350);
+        this.r1 = new Rect(50,50, 100,30);
+        this.r2 = new Rect(50,100, 100,30);
+        this.r3 = new Rect(200,50, 100,30);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        super.paint(g);
+        g2d.setPaint(Color.blue);
+        this.r1.fill(g);
+        g2d.setPaint(Color.red);
+        this.r1.paint(g);
+
+        //super.paint(g);
+        g2d.setPaint(Color.green);
+        this.r2.fill(g);
+        g2d.setPaint(Color.yellow);
+        this.r2.paint(g);
+
+        //super.paint(g);
+        g2d.setPaint(Color.black);
+        this.r3.fill(g);
+        g2d.setPaint(Color.orange);
+        this.r3.paint(g);
+        
+    }
+}
+
+class Rect {
+    int x, y;
+    int w, h;
+
+    Rect (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+    }
+
+    void fill (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.fillRect(this.x,this.y, this.w,this.h);
+    }
+
+}
