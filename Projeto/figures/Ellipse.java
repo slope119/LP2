4bb1c8e9d0a0a1f52e33e5b77bf3631a3aab0fd9
slope119 <98922380+slope119@users.noreply.github.com>
2022-04-06 03:00:48 -0300

    Create Ellipse.java

---
+++ Projeto/figures/Ellipse.java
@@ -0,0 +1,62 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
+
+public class Ellipse extends Figure {
+
+    public Ellipse (int x, int y, int w, int h, Color corl, Color corf, int difx, int dify) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.corl = corl;
+        this.corf = corf;
+        this.difx = difx;
+        this.dify = dify;
+    }
+
+    public void print () {
+        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(this.corf);
+        g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g2d.setPaint(this.corl);
+        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+    }
+    public void paint1 (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(this.corf);
+        g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g2d.setPaint(this.corl);
+        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g2d.setPaint(this.corfocus);
+        g2d.drawRect(this.x - 5,this.y - 5,this.w + 10,this.h +10);
+    }
+    public void resizeu(){
+        this.x-=5;
+        this.y-=5;
+        this.w+=10;
+        this.h+=10;
+    }
+    public void resized(){
+        this.x+=5;
+        this.y+=5;
+        this.w-=10;
+        this.h-=10;
+    }
+    public void drag(int dx, int dy){
+        if(difx == 1000 && dify == 1000){
+            difx = this.x - dx;
+            dify = this.y - dy;
+        }
+        this.x = dx + difx;
+        this.y = dy + dify;
+
+    }
+}
